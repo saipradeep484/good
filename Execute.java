296e68f3e5de1fdd1bf3f67db1e154a155bbd443
saipradeep484 <saipradeep484@gmail.com>
2024-06-18 21:10:00 +0530

    jj

---
+++ Execute.java
@@ -0,0 +1,12 @@
+class Execute{
+    public static String name()
+    {
+        if(true)
+        return "99";
+    }
+    
+    public static void main(String[] args)
+    {
+        System.out.print(name());
+    }
+}
