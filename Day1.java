1e034ef42b96c5013d79d1f3e1923305762b2d36
saipradeep484 <saipradeep484@gmail.com>
2024-06-18 12:11:02 +0530

    h

---
+++ Day1.java
@@ -0,0 +1,11 @@
+public class Day1 
+{
+    static void method(){
+        System.out.println("day1 sucks a lot");
+    }
+    public static void main(String[] args)
+    {
+         method();
+    }
+
+}
